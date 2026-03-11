e83074858f81ff539a7d3000973802b0289b79d7
Felix Eriksson <felix.eriksson1996@hotmail.com>
2026-03-11 11:28:51 +0100

    added annotation, exception and DTO description size validation

--- src/main/java/com/example/springlabb/Movies/MovieController.java
+++ src/main/java/com/example/springlabb/Movies/MovieController.java
@@ -1,9 +1,15 @@
 package com.example.springlabb.Movies;
 
+import com.example.springlabb.DTO.CreateEntityDTO;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import org.slf4j.ILoggerFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @RestController
 public class MovieController {
@@ -12,6 +18,13 @@ public class MovieController {
 
     private MovieController(MovieService movieService){
         this.movieService = movieService;
+    }
+
+    @Transactional
+    @PostMapping("/movie/createform")
+    public String createForm(@Valid CreateEntityDTO createEntityDTO, Model model, RedirectAttributes redirectAttributes){
+        //create code block for form handling
+        return null;
     }
 
 
